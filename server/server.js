c644303857b617bbc141f88abb45c5c7391b11b6
Karl Wehbe <135947812+karlwehbe@users.noreply.github.com>
2025-04-06 21:08:13 -0400

    implemented all the backend

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+
+// Start the server
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow cookies or auth headers
+};
+
+// Middleware
+app.use(cors(corsOptions)); // Enable CORS
+app.use(express.json()); // Parse JSON data in requests
+
+// Routes
+app.use("/api/auth", authRoutes); // Auth routes
+
+
